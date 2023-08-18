a20f1cfba828808ee8347487881f420875fcb9f6
Sujoy Das <das.4567@gmail.com>
2023-08-18 21:22:15 +0530

    first commit for treasury calculator class

--- treasury_calculators.py
+++ treasury_calculators.py
@@ -1,3 +1,16 @@
-class treasury_calculators():
-    pass
+class treasury_calculators:
+    def next10(self, num: float):
+        num = int(num)
+        rem = num%10
+        if rem == 0:
+            return num
+        else: return num+(10-rem)
+
+    def next100(self):
+        num = int(num)
+        rem = num%100
+        if rem == 0:
+            return num
+        else: return num+(100-rem)
+
 #create a class of calculators that have the all important functions necessary of the calculations
