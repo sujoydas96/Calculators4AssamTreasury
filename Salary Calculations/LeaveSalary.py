faa17ec43894095a521489d85d1f8c86b09c62f9
Sujoy Das <das.4567@gmail.com>
2023-08-30 11:27:37 +0530

    blueprint of program added

--- Salary Calculations/LeaveSalary.py
+++ Salary Calculations/LeaveSalary.py
@@ -4,33 +4,32 @@ from treasury_calculators import calculators
 #Functions
 
 #Global Variables
-sal_type = ""
-eGrade = 0  #employee Grade
-TotalPay = 0
-DearA = 0
-HouseR = 0
-NPS = 0
-Gross = 0
-Net = 0
 GIS = 0
 PTax = 208
-Ded = 0
+
 
 #main program
 try:
     BP = int(input("Enter the Basic Salary : "))
     GP = int(input("Enter the Grade Pay : "))
+    HRA = int(input("Enter the Percentage of House Rent Allowance : "))
+    DA = int(input("Enter the Percentage of Dearness Allowance : "))
+    MA = int(input("Enter the Medical Allowance : "))
 
     start_date = int(input("Enter the starting date of the leave period : "))        #starting date of leave date
     end_date = int(input("Enter the final date of the leave period : "))             #ending date of end date
     total_days = int(input("Enter the total number of days in the month : "))        #total number of days in the month
+    absent_days = end_date-start_date+1
 
-    TotalPay = BP+GP
-    DearA = (DA/100)*TotalPay
-    HouseR = (HRA/100)*TotalPay
+    TotalPay = ((BP+GP)/total_days)*absent_days
+    DearA = (((DA/100)*TotalPay)/total_days)*absent_days
+    HouseR = (((HRA/100)*TotalPay)/total_days)*absent_days
     Gross = TotalPay+DearA+HouseR+MA
     NPS = 0.1*(TotalPay+DearA)
     Ded = NPS+GIS+PTax
+
+    print("Gross Amount : ",Gross)
+    print("Deduction Amount : ", Ded)
 
 except:
     print("An error has occured!")
