fc250a9772354d9536998a0d82cc5aafc6bbf44a
Sujoy Das <das.4567@gmail.com>
2023-09-15 21:05:40 +0530

    Minor corrections to the program

--- Salary Calculations/Salary_Calculation.py
+++ Salary Calculations/Salary_Calculation.py
@@ -1,16 +1,4 @@
-#ISSUES
-#All calculations have to be rounded. Eg. NPS is to be rounded to its nearest whole number.
-#Calculation of GIS, Medical Allowances, Professional Taxes needs to be implemented
-#A small introduction and explanation of the program is to appear on the CLI so that users can understand the operations of the program. 
-
-#Calculation for Provitional salary. In Provisional salary, Gis is 30% for each Grade. eg. for Grade4 it will be 30, for grade3 it will be 60.
-#if more than 25000 gross than ptax is 208 otherwise 150
-#GIS  for G4 is 100, g3 is 200, g2 is 300 and g4 is 400
-#For Provisional Salary, the GIS is 30% of the actual value mentioned above.
-
-
 #Dependencies
-
 import os
 import math
 
@@ -23,10 +11,11 @@ HouseR = 0
 Gross = 0
 Net = 0
 GIS = 0
+GPF = 0
 PTax = 208
 Ded = 0
 NPS = 0
-prov_flag = 0
+custom = 0
 
 
 def GIS_calculator(level:int):
@@ -53,15 +42,16 @@ def deductions(t:int,tp:int,da:int):
         NPS = int(round(0.1*(tp+da),0))
         return NPS+GIS+PTax
     elif type == 2:  #GPF
-        print("Minimum Limits of GPF                    : ",tp*0.0625)
-        print("Maximum Limits of GPF                      : ",tp*0.156)
-        GPF_con = int(input("Enter the GPF Deduction Amount(Contribution) : "))
-        GPF_ref = int(input("Enter the GPF Deduction Amount(Refund)      : "))
-        GPF_Total = GPF_con + GPF_ref
-        print("Total GPF : ",GPF_Total)
-        return GPF_Total+GIS+PTax
+        print("Minimum Limits of GPF                        : ",round(tp*0.0625))
+        print("Maximum Limits of GPF                        : ",round(tp*0.156))
+        GPF_con = int(input("Enter the GPF Deduction Amount(Contribution) :  "))
+        GPF_ref = int(input("Enter the GPF Deduction Amount(Refund)       :  "))
+        GPF = GPF_con + GPF_ref
+        print("Total GPF : ",GPF)
+        return GPF+GIS+PTax
     elif type == 3:   #custom deductions
-        return int(input('Enter the total deduction : '))
+        custom = int(input('Enter the total deduction : '))
+        return custom
     else:
         return 0  #no deductions
 
@@ -69,7 +59,7 @@ os.system("cls")
 DA = int(input("Enter the DA percentage for the month                                          : "))
 HRA = int(input("Enter the HRA percentage for the month                                         : "))
 eGrade = int(input("Enter the Grade of the Employee(1,2,3,4)                                       : "))
-sal_type = str(input("Regular Salary/Provisional Salary? (Y/N)                                       : "))
+sal_type = str(input("Provisional Salary? (Y/N)                                                      : "))
 type = int(input("Enter the Type of Deduction (NPS-1, GPF-2, Custom Deduction-3, No Deduction-4) : "))
 MA = int(input("Enter the Medical Allowance for the month                                      : "))
 os.system("cls")
@@ -100,6 +90,7 @@ try:
 
         print("\nDearness Allowance  : ", DearA)
         print("House Rent Allowance: ", HouseR)
+        print("Medical Allowance   : ", MA)
 
         print("________________________________")
         print("Gross Salary        : ",Gross)
@@ -109,13 +100,21 @@ try:
         print("\n## DEDUCTIONS ##")
         print("_____________________________________________________________________________________")
 
-        print("NPS Contribution    : ",NPS)
+        if type == 1:
+            print("NPS Contribution    : ",NPS)
+        elif type == 2:
+            print("GPF Contribution    : ",GPF)
+        elif type == 2:
+            print("Custom Deduction    : ",custom)
+        else:
+            pass
+        
         print("GIS                 : ",GIS)
         print("Professional Tax    : ",PTax)
 
         print("________________________________")
         print("Total Deduction     : ",Ded)
-        print("Gross Salary        : ",Gross-Ded)
+        print("In-hand Salary        : ",Gross-Ded)
         print("________________________________")
 
         print("\n\n")
